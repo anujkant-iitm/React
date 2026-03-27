8f0062b3398c95ce1d88724084759e438a722c48
anujkant-iitm <anujkant.iitm@gmail.com>
2026-03-27 12:03:20 +0530

    add git ignore and first project

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './Chai.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <Chai />
+    </div>
+  )
+}
+
+export default App
---
+++ 01viteReact/src/main.jsx
@@ -0,0 +1,7 @@
+import App from './App'
+import ReactDOM from 'react-dom/client'
+// import { createRoot } from 'react-dom/client' --- IGNORE ---
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
